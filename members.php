1441c0312dc96d92148a7390a3b2b2dcce3d3d23
Celine Harrison <celinekate123@gmail.com>
2020-06-24 22:20:28 +1200

    update connection for heroku

--- members.php
+++ members.php
@@ -52,7 +52,14 @@
 
                                 <?php
 
-                                    $con = mysqli_connect('127.0.0.1','root','');
+									$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+									$server = $url["host"];
+									$username = $url["user"];
+									$password = $url["pass"];
+									$db = substr($url["path"], 1);
+
+									$conn = new mysqli($server, $username, $password, $db);
 
 									if (!$con) {
 										echo "Server error";
--- signup.php
+++ signup.php
@@ -40,7 +40,14 @@
                             <h2>Sign Up</h2>
                              
                             <?php
-                                $con = mysqli_connect('127.0.0.1','root','');
+									$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+									$server = $url["host"];
+									$username = $url["user"];
+									$password = $url["pass"];
+									$db = substr($url["path"], 1);
+
+									$conn = new mysqli($server, $username, $password, $db);
 
                                 if (!$con) {
                                     echo "Server error";
